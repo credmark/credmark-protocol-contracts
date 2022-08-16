72cfaa42e35370b4d6972f52e7454302cf67d10e
Vineet <vineet@altercode.xyz>
2022-08-16 20:26:23 +0530

    updated hardhat config for gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,11 @@
-import * as dotenv from "dotenv";
-
-import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
+import "dotenv/config";
 import "hardhat-gas-reporter";
+import { HardhatUserConfig } from "hardhat/types";
 import "solidity-coverage";
-
-dotenv.config();
 
 let accounts;
 if (process.env.ACCOUNT_MNEMONIC) {
@@ -17,24 +15,60 @@ if (process.env.ACCOUNT_MNEMONIC) {
 } else if (process.env.ACCOUNT_PRIVATE_KEY) {
   accounts = [process.env.ACCOUNT_PRIVATE_KEY];
 }
+
 const CHAIN_IDS = {
-  hardhat: 31337, 
+  hardhat: 31337,
 };
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.7",
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+      metadata: {
+        // do not include the metadata hash, since this is machine dependent
+        // and we want all generated code to be deterministic
+        // https://docs.soliditylang.org/en/v0.7.6/metadata.html
+        bytecodeHash: "none",
+      },
+    },
+  },
   networks: {
     hardhat: {
       chainId: CHAIN_IDS.hardhat,
       forking: {
         enabled: true,
-        url: `https://eth-mainnet.alchemyapi.io/v2/LilE1jthU7j0D4X6eBKbTG4Nak7MstUm`,
-      }
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      },
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
     },
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
+    enabled: !!process.env.REPORT_GAS,
     currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
