fffdef44c03a53d5c2bad0c05ca47384af566d7b
Neil Zumwalde <neilzumwalde@gmail.com>
2022-08-22 17:09:15 -0500

    fixed some math so we're counting up instead of down

--- contracts/pools/accumulators/ShareAccumulator.sol
+++ contracts/pools/accumulators/ShareAccumulator.sol
@@ -5,36 +5,42 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract ShareAccumulator is AccessControl {
 
-    mapping(address => int) public shares;
-    mapping(address => int) public accumulators;
+    mapping(address => uint) public shares;
+    mapping(address => uint) public accumulators;
     mapping(address => uint) public accumulator_ts;
 
     constructor() {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function updateShares(address account, int newShares) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function updateShares(address account, uint newShares) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(account != address(0), "CMERR: Cannot manually set global shares.");
 
         accumulators[address(0)] = totalAccumulation();
-        accumulators[account] = accumulators[address(0)] - accumulation(account);
-
         accumulator_ts[address(0)] = block.timestamp;
+
+        accumulators[account] = accumulation(account);
         accumulator_ts[account] = block.timestamp;
 
-        shares[address(0)] += newShares;
-        shares[account] += newShares;
+        shares[address(0)] += newShares - shares[account];
+        shares[account] = newShares;
     }
 
-    function advanceAccumulator(address account, int accumulation) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        accumulators[account] += accumulation;
+    function advanceAccumulator(address account, uint accumulation) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(accumulation > accumulators[account], "CMERR");
+        accumulators[account] -= accumulation;
+        accumulators[address(0)] -= accumulation;
     }
 
-    function accumulation(address account) view public returns (int) {
-        return int(block.timestamp - accumulator_ts[account]) * shares[account] - accumulators[account];
+    function accumulation(address account) view public returns (uint) {
+        return _ts_delta(account) * shares[account] + accumulators[account];
     }
 
-    function totalAccumulation() view public returns (int) {
-        return accumulators[address(0)] + accumulation(address(0));
+    function totalAccumulation() view public returns (uint) {
+        return  _ts_delta(address(0)) * shares[address(0)] + accumulators[address(0)];
+    }
+
+    function _ts_delta(address account) private view returns (uint) {
+        return block.timestamp - accumulator_ts[account];
     }
 }
--- test/12_ShareAccumulator.ts
+++ test/12_ShareAccumulator.ts
@@ -3,7 +3,7 @@ import { expect, use } from "chai";
 
 use(waffle.solidity);
 
-import { setupProtocol, MODL } from './helpers/contracts';
+import { setupProtocol, MODL, NULL_ADDRESS } from './helpers/contracts';
 import { CREDMARK_DEPLOYER, setupUsers, USER_ALICE, USER_BRENT, USER_CAMMY } from './helpers/users';
 import { ShareAccumulator, ShareAccumulator__factory } from '../typechain';
 import { advance1000Seconds, advanceADay, advanceAnHour } from './helpers/time';
@@ -24,37 +24,51 @@ describe('ShareAccumulator.sol', () => {
         testShareAccumulator = await shareAccumulatorFactory.deploy();
     });
 
+    let displayProportions = async()=>{
+        let total = await testShareAccumulator.totalAccumulation()
+        
+        console.log("alice: ", 100 * (await testShareAccumulator.accumulation(USER_ALICE.address)).toNumber() / total.toNumber(), "percent")
+        console.log("brent: ", 100 * (await testShareAccumulator.accumulation(USER_BRENT.address)).toNumber() / total.toNumber(), "percent")
+        console.log("cammy: ", 100 * (await testShareAccumulator.accumulation(USER_CAMMY.address)).toNumber() / total.toNumber(), "percent")
+    }
+
     it('ShareAccumulator.sol', async () => {
 
         await testShareAccumulator.updateShares(USER_ALICE.address, "10");
 
         await advance1000Seconds();
-        await advance1000Seconds();
-        await advance1000Seconds();
-        await advance1000Seconds();
-        await advance1000Seconds();
-        
-        console.log(await testShareAccumulator.totalAccumulation())
-        console.log(await testShareAccumulator.accumulation(USER_ALICE.address))
-        console.log(await testShareAccumulator.accumulation(USER_BRENT.address))
-        console.log(await testShareAccumulator.accumulation(USER_CAMMY.address))
+
+        await displayProportions();
 
         await testShareAccumulator.updateShares(USER_BRENT.address, "20");
 
         await advance1000Seconds();
 
-        console.log(await testShareAccumulator.totalAccumulation())
-        console.log(await testShareAccumulator.accumulation(USER_ALICE.address))
-        console.log(await testShareAccumulator.accumulation(USER_BRENT.address))
-        console.log(await testShareAccumulator.accumulation(USER_CAMMY.address))
+        await displayProportions();
 
         await testShareAccumulator.updateShares(USER_CAMMY.address, "30");
 
         await advance1000Seconds();
+        await displayProportions();
+        await advance1000Seconds();
+        await displayProportions();
+        await advance1000Seconds();
+        await displayProportions();
+        await advance1000Seconds();
+        await displayProportions();
 
+        await testShareAccumulator.updateShares(USER_CAMMY.address, "0");
+
+        await advance1000Seconds();
+        await displayProportions();
+        await advance1000Seconds();
+        await displayProportions();
+        await advance1000Seconds();
+        await displayProportions();
+        await advance1000Seconds();
+        await displayProportions();
+
+        
         console.log(await testShareAccumulator.totalAccumulation())
-        console.log(await testShareAccumulator.accumulation(USER_ALICE.address))
-        console.log(await testShareAccumulator.accumulation(USER_BRENT.address))
-        console.log(await testShareAccumulator.accumulation(USER_CAMMY.address))
     });
 })
