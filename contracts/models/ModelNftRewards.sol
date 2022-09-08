60f3782e41cfd03137f0189ae7877db5e4f07b91
Neil Zumwalde <neilzumwalde@gmail.com>
2022-09-08 13:58:51 -0500

    solc update

--- contracts/models/ModelNftRewards.sol
+++ contracts/models/ModelNftRewards.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.17;
 
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "../interfaces/IModlAllowance.sol";
