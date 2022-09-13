3bcc8a39acf928cab80733127a4ea01062dfefb5
Vineet <vineet@altercode.xyz>
2022-09-13 22:01:16 +0530

    fix: multi root model nft rewards

--- contracts/models/ModelNftRewards.sol
+++ contracts/models/ModelNftRewards.sol
@@ -12,39 +12,129 @@ contract ModelNftRewards is CModelNftRewards, IModelNftRewards {
 
     constructor(ConstructorParams memory params) CModelNftRewards(params) {}
 
-    bytes32 public merkleRoot;
-    mapping(uint256 => uint256) public claimed;
-
-    function setMerkleRoot(bytes32 root) external override manager {
-        require(merkleRoot == "", "Root already set");
-        merkleRoot = root;
-
-        emit RootUpdate(root);
+    struct Merkle {
+        bytes32 root;
+        string ipfsHash;
     }
 
-    function claimRewards(
-        uint256 tokenId,
-        uint256 amount,
-        bytes32[] memory proof
-    ) external override {
-        bytes32 leaf = keccak256(abi.encode(tokenId, amount));
+    mapping(uint256 => Merkle) public merkles;
 
-        require(
-            MerkleProof.verify(proof, merkleRoot, leaf),
-            "ModelNftRewards:PROOF_FAILED"
-        );
+    uint256 public nextMerkleIndex;
 
-        uint256 unclaimedRewards = amount - claimed[tokenId];
-        address tokenOwner = modelNft.ownerOf(tokenId);
+    mapping(uint256 => mapping(uint256 => uint256)) private claimedBitmap;
 
-        claimed[tokenId] += unclaimedRewards;
-        if (modl.balanceOf(address(this)) < unclaimedRewards) {
+    function appendRoot(bytes32 merkleRoot, string memory ipfsHash)
+        external
+        override
+        manager
+    {
+        uint256 merkleIndex = nextMerkleIndex;
+        nextMerkleIndex = merkleIndex + 1;
+
+        Merkle storage merkle = merkles[merkleIndex];
+        merkle.root = merkleRoot;
+        merkle.ipfsHash = ipfsHash;
+
+        emit RootAppend(merkleIndex, msg.sender);
+    }
+
+    /**
+     * @dev    Optimistically tries to batch together consecutive claims for the same account to
+     *         reduce gas. Therefore, the most gas-cost-optimal way to use this method
+     *         is to pass in an array of claims sorted by account.
+     */
+    function claimMulti(Claim[] memory claims) external override {
+        uint256 batchedAmount = 0;
+        uint256 claimCount = claims.length;
+        for (uint256 i = 0; i < claimCount; i++) {
+            Claim memory _claim = claims[i];
+            address tokenOwner = modelNft.ownerOf(_claim.tokenId);
+            _verifyAndMarkClaimed(tokenOwner, _claim);
+            batchedAmount += _claim.amount;
+
+            // If the next claim is NOT the same account or this claim is the last one,
+            // then disburse the `batchedAmount` to the current claim's token's account
+            uint256 nextI = i + 1;
+            if (
+                nextI == claimCount ||
+                modelNft.ownerOf(claims[nextI].tokenId) != tokenOwner
+            ) {
+                if (modl.balanceOf(address(this)) < batchedAmount) {
+                    modlAllowance.claim(address(this));
+                }
+
+                bool success = modl.transfer(tokenOwner, batchedAmount);
+                require(success, "ModelNftRewards:TRANSFER_FAILED");
+
+                batchedAmount = 0;
+            }
+        }
+    }
+
+    function claim(Claim memory _claim) external override {
+        address tokenOwner = modelNft.ownerOf(_claim.tokenId);
+        _verifyAndMarkClaimed(tokenOwner, _claim);
+
+        if (modl.balanceOf(address(this)) < _claim.amount) {
             modlAllowance.claim(address(this));
         }
 
-        bool success = modl.transfer(tokenOwner, unclaimedRewards);
-
-        emit Claim(tokenId, tokenOwner, amount);
+        bool success = modl.transfer(tokenOwner, _claim.amount);
         require(success, "ModelNftRewards:TRANSFER_FAILED");
+    }
+
+    function isClaimed(uint256 index, uint256 tokenId)
+        public
+        view
+        returns (bool)
+    {
+        uint256 claimedWordIndex = tokenId / 256;
+        uint256 claimedBitIndex = tokenId % 256;
+        uint256 claimedWord = claimedBitmap[index][claimedWordIndex];
+        uint256 mask = (1 << claimedBitIndex);
+        return claimedWord & mask == mask;
+    }
+
+    function _verifyClaim(Claim memory _claim)
+        private
+        view
+        returns (bool valid)
+    {
+        bytes32 leaf = keccak256(
+            abi.encodePacked(_claim.tokenId, _claim.amount)
+        );
+
+        return
+            MerkleProof.verify(
+                _claim.merkleProof,
+                merkles[_claim.index].root,
+                leaf
+            );
+    }
+
+    function _setClaimed(uint256 index, uint256 tokenId) private {
+        uint256 claimedWordIndex = tokenId / 256;
+        uint256 claimedBitIndex = tokenId % 256;
+        claimedBitmap[index][claimedWordIndex] =
+            claimedBitmap[index][claimedWordIndex] |
+            (1 << claimedBitIndex);
+    }
+
+    function _verifyAndMarkClaimed(address tokenOwner, Claim memory _claim)
+        private
+    {
+        require(_verifyClaim(_claim), "ModelNftRewards:INVALID_PROOF");
+        require(
+            !isClaimed(_claim.index, _claim.tokenId),
+            "ModelNftRewards:IS_CLAIMED"
+        );
+
+        _setClaimed(_claim.index, _claim.tokenId);
+        emit RewardsClaimed(
+            _claim.index,
+            _claim.tokenId,
+            tokenOwner,
+            _claim.amount
+        );
     }
 }
